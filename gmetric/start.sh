071e400c5d7d765ec10347778d14ea3c182816f2
Derek Weitzel <djw8605@gmail.com>
2011-01-12 13:34:09 -0600

    Adding idle job monitoing to gmetric subsys

--- gmetric/start.sh
+++ gmetric/start.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-running=`condor_status -schedd -format '%s\n' 'TotalRunningJobs' -const 'Name == "glidein.unl.edu"'`
+running=`/usr/bin/condor_status -schedd -format '%s\n' 'TotalRunningJobs' -const 'Name == "glidein.unl.edu"'`
+/usr/bin/gmetric --name="Running Jobs" --value="$running" --type=uint32 --units="Jobs" --tmax=600
 
-gmetric --name="Running Jobs" --value="$running" --type=uint32 --units="Jobs" --tmax=600
+
+idle=`/usr/bin/condor_status -schedd -const 'Name == "glidein.unl.edu"' -format '%s\n' 'TotalIdleJobs'`
+/usr/bin/gmetric --name="Idle Jobs" --value="$idle" --type=uint32 --units="Jobs" --tmax=600
+
 
